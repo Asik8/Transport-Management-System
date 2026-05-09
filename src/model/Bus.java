082b2b58f42c37d54b8df29251ff1dd2f517459b
Asik8 <motasembillah84@gmail.com>
2026-05-09 23:25:44 +0600

    Bus class attributes and mothods implemented

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -2,4 +2,18 @@ package model;
 
 public class Bus extends Vehicle {
 
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public double calculateFair() {
+        return 0;
+    }
 }
