23893c7877b065e1b4b0dcabd3fe8449337aab19
Asik8 <motasembillah84@gmail.com>
2026-05-09 19:28:53 +0600

    Vehicle class attributes and mothods implemented

--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -1,4 +1,56 @@
 package model;
 
-public class Vehicle {
+/*
+* Abstract parent class Vehicle
+ */
+public abstract class Vehicle {
+    private String id;
+    private int capacity;
+    private double speed;
+    private Route route;
+
+    // No Argument Constructor
+    public Vehicle() {
+    }
+
+    // All Argument Constructor
+    public Vehicle(String id, int capacity, double speed, Route route) {
+        this.id = id;
+        this.capacity = capacity;
+        this.speed = speed;
+        this.route = route;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
 }
