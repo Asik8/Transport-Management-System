856257c45795b9f843deb7fbd22b67aae3f55b94
Asik8 <motasembillah84@gmail.com>
2026-05-11 20:22:32 +0600

    Train class Created

--- src/model/Train.java
+++ src/model/Train.java
@@ -1,4 +1,36 @@
 package model;
 
-public class Train {
+public class Train extends Vehicle {
+    // Override Methods
+    @Override
+    public void start() {
+        System.out.println("Train started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Train stopped");
+    }
+
+    @Override
+    public double calculateFair() {
+        double distance=getRoute().getDistance();
+        return distance*3;
+    }
+
+    @Override
+    public void details(){
+        super.details();
+        System.out.println("Train Fair: "+calculateFair());
+    }
+
+    // No Argument Constructor
+
+    public Train() {
+    }
+
+    // All Argument Constructor
+    public Train(String id, int capacity, double speed, Route route) {
+        super(id, capacity, speed, route);
+    }
 }
--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -59,7 +59,7 @@ public abstract class Vehicle {
     public abstract void stop();
     public abstract double calculateFair();
 
-    public void Details(){
+    public void details(){
         System.out.println("ID: " + id);
         System.out.println("Capacity: " + capacity);
         System.out.println("Speed: " + speed);
