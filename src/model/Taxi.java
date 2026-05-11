3b4dd536f78a24fadd3a59a50a2a05ff41f4cb84
Asik8 <motasembillah84@gmail.com>
2026-05-11 20:16:27 +0600

    Taxi class Created

--- src/model/Taxi.java
+++ src/model/Taxi.java
@@ -2,6 +2,29 @@ package model;
 
 public class Taxi extends Vehicle {
 
+    // Override Methods
+    @Override
+    public void start() {
+        System.out.println("Taxi is started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Taxi is stopped");
+    }
+
+    @Override
+    public double calculateFair() {
+        double distance = getRoute().getDistance();
+        return 50+(distance*15);
+    }
+
+    @Override
+    public void Details() {
+        super.Details();
+        System.out.println("Total Fair: "+calculateFair());
+    }
+
     // No Argument Constructor
     public Taxi() {
     }
